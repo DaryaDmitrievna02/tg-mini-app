866664768a7106beefa524ce8ad709a028880a63
Darya <135708765+DaryaDmitrievna02@users.noreply.github.com>
2024-08-19 09:11:58 +0300

    Update bot.js

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,8 +1,8 @@
 const { Telegraf } = require("telegraf");
-const TOKEN = "7204755708:AAHcTTbe_LBu9WtNMR3dPN_itrzH6hG4ozk";
+const TOKEN = "#token";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://66c26e70b5731c0006cdc0a9--fastidious-bonbon-034523.netlify.app/";
+const web_link = "#web_site";
 
 bot.start((ctx) =>
   ctx.reply("Привет :))", {
